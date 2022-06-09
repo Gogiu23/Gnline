6385c5bae5c4739cdc8b53e5c6467041c4181c95
giuliano dominici <giulianodominici@MacBook-Pro-3.local>
2022-06-09 18:04:17 +0200

    beginning

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,14 @@
+
+#include "get_next_line.h"
+
+size_t	ft_strlen_next(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
