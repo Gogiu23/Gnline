6e658f32fe0cef8950c87b5a66fe7bfa0fc14fb2
giuliano dominici <giuliandominici@gmail.com>
2022-06-15 06:33:27 +0200

    to campus

--- get_next_line.c
+++ get_next_line.c
@@ -11,18 +11,25 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-char	*get_line(char *ptr, unsigned int tot)
+char	*get_line(char *ptr)
 {
 	char  *line;
-	
-	while (ptr[tot] != '\n' || ptr[tot] == '\0')
+	int	  count;
+
+	count = 0;
+	while (ptr[count] && ptr[count] == '\n')
+		count++;
+	line = (char *)malloc(sizeof(char) * count + 2);
+	if (!line)
+		return (NULL);
+	while (ptr[count] && ptr[count] != '\n')
 	{
-		printf("Valor de line: %s\n", line);
-		line = ft_strchr(ptr, '\n');
-		printf("Valor de line: %s\n", line);
-		printf("Valor de ptr: %s\n", ptr);
-		tot--;
+		line[count] = ptr[count];
+		count++;
 	}
+//	line = ft_strtrim(ptr, ft_strchr(ptr, '\n'));
+	printf("Valor de line: %s\n", line);
+	printf("Valor de ptr: %s\n", ptr);
 	printf("Valor de line: %s\n", line);
 	return (line);
 }
@@ -34,27 +41,30 @@ char	*get_next_line(int fd)
 	int				nbytes;
 	static char		*ptr;
 	int				i;
-	int				tot;
 
 	i = 0;
-	nbytes = 0;
+	nbytes = 1;
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (0);
 	
-	while (!(ft_strchr(buffer, '\n') && nbytes == 0))
+	while (!ft_strchr(buffer, '\n') && nbytes != 0)
 	{
 		printf("Valor de buffer antes del read: %s\n", buffer);
 		printf("Valor de nbytes antes del read: %d\n", nbytes);
 		nbytes = read(fd, buffer, BUFFER_SIZE);
+		if (nbytes == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[nbytes] = '\0';
 		ptr = ft_strjoin(ptr, buffer);
 		i++;
 		printf("Contenido de ptr dentro del while: %s\n", ptr);
 		printf("Vueltas dentro del while: %d\n", i);
 	}
-	tot = i * BUFFER_SIZE;
-	printf("valor de tot: %d\n", tot);
 	free(buffer);
-	line = get_line(ptr, tot);
+	line = get_line(ptr);
 	return (line);
 }
