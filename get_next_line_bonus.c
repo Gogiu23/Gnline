77dfc99b3105004c56dd5336940668ad834a64e1
Giuliano Dominici <gdominic@student.42barcelona.com>
2022-06-25 20:31:31 +0200

    files bonus created, stopped at buffer'malloc

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,22 +1,36 @@
 #include "get_next_line_bonus.h"
 
+char	*get_next_line_bonus(int fd)
+{
+	char			*buffer;
+	static char		*ptr;
+	int				nbytes;
+
+	nbytes = 1;
+	buffer = (char *)malloc(BUFFFER_SIZE + 1) * sizeof(char);
+	if (!buffer)
+		return (NULL);
+}
+
 int main()
 {
-	int fd2;
-	int fd;
+	int		fd;
+	int		fd2;
+	int		fd3;
+	char	line;
 
-	fd = open("bonus.txt", O_CREAT);
+	fd = open("bonus.txt", O_CREAT | O_RDWR);
+	dprintf(fd, "Hola\n\n\nMe llamo Giuli");
+	fd2 = open("bonus2.txt", O_CREAT | O_RDWR);
+	dprintf(fd2, "Adios\n");
+	fd3 = open("bonus3.txt", O_CREAT | O_RDWR);
+	dprintf(fd3, "C'era una volta un giovane intreprendente\nche voleva a tutti i costi diventare principe di Idalgo");
+	line = get_next_line_bonus(fd);
+	printf("%s\n", line);
+	free(line);
 	close(fd);
-	fd = open("bonus.txt", O_RDWR);
-	dprintf(3, "Hola\n\n\nMe llamo Giuli");
-	close(fd);
-	fd2 = open("bonus2.txt", O_CREAT);
 	close(fd2);
-	fd2 = open("bonus2.txt", O_RDWR);
-	dprintf(-1, "Adios\n");
-	printf("valor de fd: %d\n", fd);
-	printf("Valor de fd2: %d\n", fd2);
-	close(fd2);
+	close(fd3);
 	return (0);
 }
 
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,8 +1,14 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+#endif
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+
+char	*get_next_line_bonus(int fd);
 
 #endif
